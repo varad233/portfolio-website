c53a30acdd4caebae0a8a53034266e6d942088f7
varad233 <136105601+varad233@users.noreply.github.com>
2024-08-10 23:39:06 +0530

    changes

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def certificates():
 def club_activities():
     return render_template('club_activities.html')
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
